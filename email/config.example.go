6b1680cfe8a4c2f8c2fbe055d0cc6b8b5bc97eb3
ysholqamy <usfhussam@live.com>
2018-05-22 05:14:41 +0200

    provider keys are now read through env

--- email/config.example.go
+++ email/config.example.go
@@ -1,10 +1,12 @@
 package email
 
-const (
-	mailgunBaseURL = "https://api.mailgun.net/v3"
-	mailgunDomain  = "sandbox414000e0f08e4f04bf93a2a5c15736c1.mailgun.org"
-	mailgunKey     = "key-75e3c513a164c7039855fbd074f38641"
+import "os"
 
-	sendgridBaseURL = "https://api.sendgrid.com/api/mail.send.json"
-	sendgridKey     = "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"
+var (
+	mailgunBaseURL = os.Getenv("MAILGUN_BASE_URL")
+	mailgunDomain  = os.Getenv("MAILGUN_DOMAIN")
+	mailgunKey     = os.Getenv("MAILGUN_KEY")
+
+	sendgridBaseURL = os.Getenv("SENDGRID_BASE_URL")
+	sendgridKey     = os.Getenv("SENDGRID_KEY")
 )
